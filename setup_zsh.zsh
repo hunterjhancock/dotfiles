6123ec1216dcda02fec95f53494a4db1165e6434
Hunter Hancock <55824161+hunterjhancock@users.noreply.github.com>
2022-07-20 10:39:32 -0700

    Symlink sh to zsh (pre-installed, not Homebrew)

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -8,12 +8,24 @@ if grep -Fxq '/usr/local/bin/zsh' '/etc/shells'; then
     echo '/usr/local/bin/zsh already exists in etc/shells'
 else
     # echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+    echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 fi
 
 if [ "$SHELL" = '/usr/local/bin/zsh' ]; then
     echo '$SHELL is already /usr/local/bin/zsh'
 else 
-echo "Enter user password to change login shell"
-chsh -s '/usr/local/bin/zsh'
+    echo "Enter user password to change login shell"
+    chsh -s '/usr/local/bin/zsh'
+fi
+
+# This step is overkill, honestly - might delete
+if sh --version | grep -q zsh; then
+    echo '/private/var/select/sh already linked to /bin/zsh'
+else
+    echo "Enter superuser (sudo) password to symlink sh to zsh"
+    sudo ln -sfv /bin/zsh /private/var/select/sh
+
+    # I'd like to run this command instead, which uses Homebrew zsh
+    # instead of the Apple default installation of zsh:
+    # sudo ln -sfv /usr/local/bin/zsh /private/var/select/sh
 fi
